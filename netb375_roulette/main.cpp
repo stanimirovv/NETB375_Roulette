77f94be4ec76ea6204f21fc2c20ba991da0db27f
Bloodcount <searosss@gmail.com>
2014-11-10 16:07:16 +0200

    added test db connection

--- netb375_roulette/main.cpp
+++ netb375_roulette/main.cpp
@@ -1,5 +1,9 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QSqlDatabase>
+#include <QtSql>
+#include <QDebug>
+#include <QString>
 
 int main(int argc, char *argv[])
 {
@@ -7,5 +11,23 @@ int main(int argc, char *argv[])
     MainWindow w;
     w.show();
 //dashov comment
+    //QSqlDatabase db = QSqlDatabase::addDatabase(“QPSQL”);
+    QSqlDatabase db = QSqlDatabase::addDatabase("QPSQL");
+    db.setHostName("localhost");
+    db.setDatabaseName("nykal");
+    db.setUserName("nykal");
+    db.setPassword("123");
+    db.open(); //  return a.exec();
+    QSqlQuery query(db);
+    query.prepare("SELECT now() as tt1;");
+    query.exec();
+    int index = query.record().indexOf("tt1");
+    while (query.next())
+    {
+        QString time = query.value(index).toString();
+        qDebug() << "Qdebug:r" << time;
+    }
+
+    db.close();
     return a.exec();
 }
--- netb375_roulette/netb375_roulette.pro
+++ netb375_roulette/netb375_roulette.pro
@@ -5,6 +5,8 @@
 #-------------------------------------------------
 
 QT       += core gui
+QT += sql
+QT += core
 
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 
