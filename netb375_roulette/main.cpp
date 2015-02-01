aaf39e1a2d68617ffbb9efb7ed06a3dd428615b8
AleksandarDashov <GrimReaperBG20@gmail.com>
2015-02-01 16:37:51 +0200

    Update

--- netb375_roulette/main.cpp
+++ netb375_roulette/main.cpp
@@ -1,4 +1,5 @@
 #include "mainwindow.h"
+#include "startwindow.h"
 #include <QApplication>
 #include <QDebug>
 
@@ -6,7 +7,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
+    StartWindow w;
     w.show();
 
     return a.exec();
