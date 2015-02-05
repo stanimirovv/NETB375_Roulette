81e04830b5e07f79bf2c3f2bf3db93224d0cb5b5
Aleksandar Dashov <GrimReaperBG20@gmail.com>
2015-02-05 16:20:54 +0200

    LoadGame
    When Load Game button is clicked the new interface pops up

--- netb375_roulette/startwindow.cpp
+++ netb375_roulette/startwindow.cpp
@@ -7,7 +7,9 @@ StartWindow::StartWindow(QWidget *parent) :
     ui(new Ui::StartWindow)
 {
     ui->setupUi(this);
+    this->move(800, 250);//window positioning
     setStyleSheet("StartWindow {background-color:green};");
+
 }
 
 StartWindow::~StartWindow()
@@ -22,7 +24,15 @@ void StartWindow::on_pushButton_new_clicked()
     setprof->show();
 }
 
+void StartWindow::on_pushButton_load_clicked()
+{
+    load = new LoadGame();
+    load->show();
+}
+
+
 void StartWindow::on_pushButton_quit_clicked()
 {
     QApplication::quit();
 }
+
